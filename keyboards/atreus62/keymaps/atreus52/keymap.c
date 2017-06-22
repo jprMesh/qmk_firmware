f1b1a3484788ba4bf1261223f7a88570cb0f0bdf
Jonas <jprogers@mitre.org>
2017-06-22 16:32:50 +0000

    Atreus52- Modified BDO layer and added momentary layer for chatting.

--- keyboards/atreus62/keymaps/atreus52/keymap.c
+++ keyboards/atreus62/keymaps/atreus52/keymap.c
@@ -6,7 +6,8 @@
 #define RAISE 2
 #define LOWER 3
 #define BDO 4
-#define RESETL 5
+#define CHAT 5
+#define RESETL 6
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [DVORAK] = KC_KEYMAP(
@@ -28,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     NO, TILD, GRV,  LCBR, RCBR, DQUO,             QUOT, EQL,  PLUS, MINS, QUES, NO, \
     NO, ESC,  TAB,  LPRN, RPRN, BSLS,             SLSH, LEFT, DOWN, UP,   RGHT, NO, \
     NO, TRNS, TRNS, LBRC, RBRC, TRNS,             INS,  PIPE, UNDS, TRNS, TRNS, NO, \
-    NO, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, FN3,  NO  ),
+    NO, FN3,  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, DOWN, UP,   NO  ),
 
 [LOWER] = KC_KEYMAP(
     NO, TRNS, TRNS, TRNS, TRNS, TRNS,             TRNS, TRNS, TRNS, TRNS, TRNS, NO, \
@@ -38,11 +39,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     NO, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, NO  ),
 
 [BDO] = KC_KEYMAP(
-    NO, ESC,  1,    2,    3,    4,                5,    0,    SLSH, U,    C,    NO, \
-    NO, TAB,  Q,    W,    E,    R,                6,    Y,    I,    O,    P,    NO, \
-    NO, LSFT, A,    S,    D,    F,                7,    G,    H,    J,    K,    NO, \
-    NO, T,    Z,    X,    C,    V,                8,    B,    N,    M,    L,    NO, \
-    NO, LCTL, SPC,  SPC,  SPC,  SPC,  COMM, ENT,  9,    NO,   NO,   NO,   FN2,  NO  ),
+    NO, ESC,  2,    3,    4,    5,                6,    NO,   NO,   NO,   NO,   NO, \
+    NO, 1,    Q,    W,    E,    R,                7,    Y,    I,    O,    P,    NO, \
+    NO, LSFT, A,    S,    D,    F,                8,    G,    H,    J,    K,    NO, \
+    NO, T,    O,    I,    M,    P,                9,    B,    N,    M,    L,    NO, \
+    NO, LCTL, COMM, SPC,  FN4,  SPC,  TAB,  ENT,  0,    NO,   U,    NO,   FN2,  NO  ),
+
+[CHAT] = KC_KEYMAP(
+    NO, 1,    2,    3,    4,    5,                6,    7,    8,    9,    0,    NO, \
+    NO, QUOT, COMM, DOT,  P,    Y,                F,    G,    C,    R,    L,    NO, \
+    NO, A,    O,    E,    U,    I,                D,    H,    T,    N,    S,    NO, \
+    NO, SCLN, Q,    J,    K,    X,                B,    M,    W,    V,    Z,    NO, \
+    NO, NO,   NO,   NO,   TRNS, LSFT, BSPC, ENT,  SPC,  FN0,  NO,   NO,   NO,   NO  ),
 
 [RESETL] = KEYMAP(
     KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
@@ -57,7 +65,8 @@ const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(RAISE), // Raise layer
     [1] = ACTION_LAYER_MOMENTARY(LOWER), // Lower layer
     [2] = ACTION_LAYER_TOGGLE(BDO), // BDO layer
-    [3] = ACTION_LAYER_TOGGLE(RESETL) // RESET layer
+    [3] = ACTION_LAYER_TOGGLE(RESETL), // RESET layer
+    [4] = ACTION_LAYER_MOMENTARY(CHAT) // CHAT layer for BDO
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -93,7 +102,7 @@ bool process_record_user (uint16_t keycode, keyrecord_t *record) {
     }
 
     if (qw_dv_swap_state == 0b00000011) {
-        layer_invert(DVORAK);
+        layer_invert(QWERTY);
     }
     return true;
 }
